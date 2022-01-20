3b17b4b5234c0a6a794929ce7568d9203d51b312
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-20 16:43:01 -0700

    Set up fonts and universal style sheet

---
+++ page-site-styles.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<h1>Hello world!</h1>
+<h2>Hello world!</h2>
+<h3>Hello world!</h3>
+<h4>Hello world!</h4>
+<h5>Hello world!</h5>
+<h6>Hello world!</h6>
+
+<p>Lorem ipsum doller met.</p>
+
+<a class="rev-button">Test button</a>
+
+<div class="rev-card">
+    <h1>Test card title</h1>
+    <p>Test card body text.</p>
+    <form action="">
+        <label for="">Test card input label</label>
+        <input type="text" placeholder="Test card text input">
+    </form>
+    <a class="rev-button">Test card button</a>
+</div>
+
+<label for="">Category drop-down</label>
+<select name="">
+    <option value="">Test Category 1</option>
+    <option value="">Test Category 1</option>
+</select>
+
+<?php get_footer(); ?>
