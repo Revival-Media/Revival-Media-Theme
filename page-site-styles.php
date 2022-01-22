e4d654af4130f4fb5ac8b8e8e1234e3785434314
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-22 08:49:38 -0700

    Created test page to work on site-wide styles.

--- page-site-styles.php
+++ page-site-styles.php
@@ -1,30 +1,38 @@
 <?php get_header(); ?>
 
-<h1>Hello world!</h1>
-<h2>Hello world!</h2>
-<h3>Hello world!</h3>
-<h4>Hello world!</h4>
-<h5>Hello world!</h5>
-<h6>Hello world!</h6>
+<section class="site-width top-sec">
+    <h1>Hello world! <span class="grad-text">Gradient text test</span> 👋</h1>
+    <h2>Hello world!</h2>
+    <h3>Hello world!</h3>
+    <h4>Hello world!</h4>
+    <h5>Hello world!</h5>
+    <h6>Hello world!</h6>
 
-<p>Lorem ipsum doller met.</p>
+    <p>Lorem ipsum doller met.</p>
 
-<a class="rev-button">Test button</a>
+    <a class="rev-button">Test button</a>
 
-<div class="rev-card">
-    <h1>Test card title</h1>
-    <p>Test card body text.</p>
-    <form action="">
-        <label for="">Test card input label</label>
-        <input type="text" placeholder="Test card text input">
-    </form>
-    <a class="rev-button">Test card button</a>
-</div>
+    <div class="rev-card">
+        <h1>Test card title</h1>
+        <p>Test card body text.</p>
+        <form class="contact-form" action="">
+            <div class="field">
+                <label for="">Test card input label</label>
+                <input type="text" placeholder="Test card text input">
+            </div>
+            <div class="field">
+                <label for="">Test card input label</label>
+                <input type="text" placeholder="Test card text input">
+            </div>
+        </form>
+        <a class="rev-button">Test card button</a>
+    </div>
 
-<label for="">Category drop-down</label>
-<select name="">
-    <option value="">Test Category 1</option>
-    <option value="">Test Category 1</option>
-</select>
+    <select name="">
+        <option value="">Select Service</option>
+        <option value="">Test Category 1</option>
+        <option value="">Test Category 1</option>
+    </select>
+</section>
 
 <?php get_footer(); ?>
