181edcae4258767748b858d7a8062f5532c165cf
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-24 12:29:01 -0700

    Uploaded more services icon and image assets. Built the HTML structure and styled the services page.

--- page-services.php
+++ page-services.php
@@ -1,9 +1,155 @@
 <?php get_header(); ?>
 
-<section class="site-width" style="display: grid; grid-template-columns: 1fr 1fr; align-items: start;">
-        <h1 style="padding-bottom: 2rem; width: 35ch;">
+<section class="site-width services-hero-section">
+        <h1>
             <span class="grad-text">Our services</span> build brands that connect with consumers and  <span class="grad-text">drives growth</span> 🚀
         </h1>
+</section>
+<section class="site-width services-block">
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/development-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/assets/imgs/web-dev-icon.svg'; ?>" alt="Development">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Web Development</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/social-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/assets/imgs/social-icon.svg'; ?>" alt="Social">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Social media marketing</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/design-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img style="transform: scale(0.7);" src="<?php echo get_template_directory_uri() . '/assets/imgs/print-icon.svg'; ?>" alt="Print">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Print and digital design</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/build-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-build-icon.svg'; ?>" alt="Brand">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Brand building</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/copywriting-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/assets/imgs/copywriting-icon.svg'; ?>" alt="Copywriting">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Voice and copywriting</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="rev-card">
+        <img class="service-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/security-service.jpg'; ?>" alt="Strategy">
+        <div class="service-text">
+            <div class="flex-wrap">
+                <img src="<?php echo get_template_directory_uri() . '/assets/imgs/security-icon.svg'; ?>" alt="Security">
+                <h3 style="margin-right: auto; margin-left: 2rem;">Maintenance and security</h3>
+            </div>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
+                <div style="margin-right: 2.5rem">
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+                <div>
+                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Social and web strategy</span></li>
+                    <li class="rev-bullet"><span>Brand driven proposals</span></li>
+                </div>
+            </ul>
+        </div>
+    </div>
 </section>
 
 <?php get_template_part('template-parts/contact-form'); ?>
