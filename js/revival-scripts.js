02a26cedee2857d6a438d0b1be9ba30e2ab72638
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-24 10:22:28 -0700

    Removed button script from revival-scripts.js file.

--- js/revival-scripts.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $(function() {
-      $('a.rev-button').each(function(){
-        wid = $(this).width() + 65;
-        $(this).css('width', wid+'px');
-      });
-    });
-  })(jQuery);
