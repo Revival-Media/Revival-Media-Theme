9c96dfb7a47a47391503664a1c02d16369bc3325
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-24 10:21:56 -0700

    Added home page styles and modified home page html.

--- front-page.php
+++ front-page.php
@@ -10,33 +10,48 @@
         <a class="rev-button" style="margin-top: 4rem;">Learn About Us</a>
 </section>
 <section>
+
     <div id="hero-card-wrap" class="rev-card-wrap" style="display: flex; position: relative; margin: 0 auto; padding: 0 0 0 5rem; max-width: 1400px; overflow-y: auto;">
-        <div class="rev-card hero-card" style="display: flex; flex-wrap: wrap; justify-content: space-between; align-items: end; margin-right: 10px;">
-            <div class="hero-card-title">
-                <h4>Precision Prep</h4><br>
-                <h2>Shopify Website</h2>
-            </div>
-            <a class="rev-button">Discover More</a>
+        <?php 
+            // Define Query Parameters
+            $home_posts = new WP_Query( 'tag=homepage' );
+        ?>
+            
+        <?php 
+            // Start WP Query
+            while ($home_posts -> have_posts()) : $home_posts -> the_post(); 
+            // Get Project Name
+            $projectName = get_post_meta(get_the_ID(), 'Project', true);
+            //Get Client Name
+            $clientName = get_post_meta(get_the_ID(), 'Client', true);
+            // Display the Project Title and Client with Hyperlink
+        ?>
+
+        <div class="rev-card hero-card" id="card-<?php echo $post->ID; ?>">
+            <a href="<?php the_permalink() ?>">
+                <h4><?php echo $projectName; ?></h4>
+                <h2><?php echo $clientName; ?></h2>
+                <a href="#" class="rev-button">Discover More</a>
+            </a>
         </div>
-        <div class="rev-card hero-card" style="display: flex; flex-wrap: wrap; justify-content: space-between; align-items: end; margin-right: 10px;">
-            <div class="hero-card-title">
-                <h4>Precision Prep</h4><br>
-                <h2>Shopify Website</h2>
-            </div>
-            <a class="rev-button">Discover More</a>
-        </div>
+        
+        <?php 
+            // Repeat the process and reset once it hits the limit
+            endwhile;
+            wp_reset_postdata();
+        ?>
     </div>
 </section>
 <section class="site-width">
-    <div class="rev-card">
-        <div class="flex-wrap" style="margin-bottom: 1.5rem;">
+    <div class="rev-card services-card">
+        <div class="flex-wrap">
             <h3 class="grad-text">Our services</h3>
             <a class="rev-button">Learn More</a>
         </div>
-        <h2 style="margin-bottom: 3rem; max-width: 44ch;">
+        <h2 style="max-width: 44ch;">
             Our integrated <span class="grad-text">web, social, print and digital</span> services make brands work for business. 
         </h2>
-        <div class="services-wrap" style="display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;">
+        <div class="services-wrap">
             <div class="services-block">
                 <div class="flex-wrap">
                     <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/magnify-icon.svg'; ?>" alt="Strategy">
@@ -51,7 +66,7 @@
             </div>
             <div class="services-block">
                 <div class="flex-wrap">
-                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-icon.svg'; ?>" alt="Strategy">
+                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-icon.svg'; ?>" alt="Brand">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Brand</h3>
                 </div>
                 <ul style="margin-top: 2rem; line-height: 2.5rem">
@@ -63,7 +78,7 @@
             </div>
             <div class="services-block">
                 <div class="flex-wrap">
-                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/digital-icon.svg'; ?>" alt="Strategy">
+                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/digital-icon.svg'; ?>" alt="Digital">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Digital</h3>
                 </div>
                 <ul style="margin-top: 2rem; line-height: 2.5rem">
@@ -75,8 +90,8 @@
             </div>
             <div class="services-block">
                 <div class="flex-wrap">
-                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-icon.svg'; ?>" alt="Strategy">
-                    <h3 style="margin-right: auto; margin-left: 2rem;">Brand</h3>
+                    <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/print-icon.svg'; ?>" alt="Strategy">
+                    <h3 style="margin-right: auto; margin-left: 2rem;">Print</h3>
                 </div>
                 <ul style="margin-top: 2rem; line-height: 2.5rem">
                     <li class="rev-bullet"><span>Innovative creative concepts</span></li>
@@ -88,22 +103,33 @@
         </div>
     </div>
 </section>
-<section class="site-width" style="display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr 1fr 1fr; align-items: center; gap: 7.5rem 5rem;">
-    <div class="rev-card" style="min-height: 40rem;"></div>
-    <div class="card-image-wrap">
-        <h3>Bespoke customer journeys with expert <span class="grad-text">Shopify design and development.</span></h3>
-        <a class="rev-button">View Projects</a>
+<section class="site-width card-text-section">
+    <div class="image-text-wrap">
+        <div class="rev-card"></div>
+        <div class="card-image-wrap">
+            <h3>Bespoke customer journeys with expert <span class="grad-text">Shopify design and development.</span></h3>
+            <a class="rev-button">View Projects</a>
+        </div>
     </div>
-    <div style="text-align: right;">
-        <h3><span class="grad-text">Wordpress sites</span> to showcase your business and boost online enagement.</h3>
-        <a class="rev-button">View Projects</a>
+    <div class="image-text-wrap card-switch">
+        <div style="text-align: right;" class="card-image-wrap">
+            <h3><span class="grad-text">Wordpress sites</span> to showcase your business and boost online enagement.</h3>
+            <a class="rev-button">View Projects</a>
+        </div>
+        <div class="rev-card"></div>
     </div>
-    <div class="rev-card" style="min-height: 40rem;"></div>
-    <div class="rev-card" style="min-height: 40rem;"></div>
-    <div>
-        <h3>Straight-talking social media marketing <span class="grad-text">for ambitious brands.</span></h3>
-        <a class="rev-button">View Projects</a>
+    <div class="image-text-wrap">
+        <div class="rev-card"></div>
+        <div class="card-image-wrap">
+            <h3>Straight-talking social media marketing <span class="grad-text">for ambitious brands.</span></h3>
+            <a class="rev-button">View Projects</a>
+        </div>
     </div>
+</section>
+<section class="site-width">
+    <h2>
+        <span class="grad-text">Our clients</span> speak for themselves.
+    </h2>
 </section>
 
 <?php get_template_part('template-parts/contact-form'); ?>
