c9a9855fe06ca69c24e99966b374bc3bebb9fc67
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-24 10:20:05 -0700

    Created the contact form template part.

---
+++ template-parts/contact-form.php
@@ -0,0 +1,28 @@
+<section class="site-width">
+    <div class="rev-card contact-card" style="display: grid; grid-template-columns: auto 1fr; padding: 0; overflow: hidden;">
+        <div style="padding: 5rem;">
+            <h3>Like the idea of working with us?</h3>
+            <h1>Book an <span class="grad-text">intro call</span> today.</h1>
+            <form class="contact-form contact-form-template" action="" style="margin-bottom: 0 !important;">
+                <div class="field">
+                    <label for="">Name *</label>
+                    <input type="text" placeholder="Test card text input">
+                </div>
+                <div class="field">
+                    <label for="">Email *</label>
+                    <input type="text" placeholder="Test card text input">
+                </div>
+                <div class="field">
+                    <label for="">Job Title</label>
+                    <input type="text" placeholder="Test card text input">
+                </div>
+                <div class="field">
+                    <label for="">Company</label>
+                    <input type="text" placeholder="Test card text input">
+                </div>
+                <a class="rev-button">Send</a>
+            </form>
+        </div>
+        <div style="background-color: pink;"></div>
+    </div>
+</section>
