b8a3cd38266d6e8bc380ee41a368fe7a8db6bdcb
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-28 16:38:12 -0700

    Images uploaded.

--- front-page.php
+++ front-page.php
@@ -11,7 +11,12 @@
 </section>
 <section>
 
-    <div id="hero-card-wrap" class="rev-card-wrap" style="display: flex; position: relative; margin: 0 auto; padding: 0 0 0 5rem; max-width: 1400px; overflow-y: auto;">
+   
+
+    <div id="hero-card-wrap" class="rev-card-wrap hp-slider" style="display: flex; position: relative; margin: 0 auto; padding: 0 0 0 5rem; max-width: 1400px; overflow: visible;">
+        
+        <div class="blank"></div>
+        
         <?php 
             // Define Query Parameters
             $home_posts = new WP_Query( 'tag=homepage' );
@@ -24,16 +29,40 @@
             $projectName = get_post_meta(get_the_ID(), 'Project', true);
             //Get Client Name
             $clientName = get_post_meta(get_the_ID(), 'Client', true);
+            //Get Background Colour
+            $background = get_post_meta(get_the_ID(), 'Background', true);
+            //Get textd Colour
+            $textcolor = get_post_meta(get_the_ID(), 'Text Colour', true);
+             
             // Display the Project Title and Client with Hyperlink
         ?>
 
-        <div class="rev-card hero-card" id="card-<?php echo $post->ID; ?>">
+        <div style="background-color: <?php echo $background; ?>;" class="rev-card hero-card" id="card-<?php echo $post->ID; ?>">
             <a href="<?php the_permalink() ?>">
-                <h4><?php echo $projectName; ?></h4>
-                <h2><?php echo $clientName; ?></h2>
+                <div class="card-info-wrap">
+                    <h4 style="color: <?php echo $textcolor; ?>;"><?php echo $clientName; ?></h4>
+                    <h2 style="color: <?php echo $textcolor; ?>;"><?php echo $projectName; ?></h2>
+                    <a href="#" class="rev-button">Discover More</a>
+                </div>
+            </a>
+            
+            <div class="screen-wrap">
+                <div class="screen-ui">
+                    <div class="red screen-ui-btn"></div>
+                    <div class="yellow screen-ui-btn"></div>
+                    <div class="green screen-ui-btn"></div>
+                </div>
+                <div class="screenshot"></div>
+            </div>
+        </div>
+
+        <!--<div class="rev-card hero-card" id="card-<?php echo $post->ID; ?>">
+            <a href="<?php the_permalink() ?>">
+                <h4><?php echo $clientName; ?></h4>
+                <h2><?php echo $projectName; ?></h2>
                 <a href="#" class="rev-button">Discover More</a>
             </a>
-        </div>
+        </div>-->
         
         <?php 
             // Repeat the process and reset once it hits the limit
@@ -41,18 +70,17 @@
             wp_reset_postdata();
         ?>
     </div>
+
 </section>
 <section class="site-width">
     <div class="rev-card services-card">
-        <div class="flex-wrap">
-            <h3 class="grad-text">Our services</h3>
-            <a class="rev-button">Learn More</a>
-        </div>
+        <h3 class="grad-text">Our services</h3>
         <h2 style="max-width: 44ch;">
             Our integrated <span class="grad-text">web, social, print and digital</span> services make brands work for business. 
         </h2>
+        <a class="rev-button">Learn More</a>
         <div class="services-wrap">
-            <div class="services-block">
+            <div class="home-services-block">
                 <div class="flex-wrap">
                     <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/magnify-icon.svg'; ?>" alt="Strategy">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Strategy</h3>
@@ -64,7 +92,7 @@
                     <li class="rev-bullet"><span>Brand driven proposals</span></li>
                 </ul>
             </div>
-            <div class="services-block">
+            <div class="home-services-block">
                 <div class="flex-wrap">
                     <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-icon.svg'; ?>" alt="Brand">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Brand</h3>
@@ -76,7 +104,7 @@
                     <li class="rev-bullet"><span>Pixel perfect logos</span></li>
                 </ul>
             </div>
-            <div class="services-block">
+            <div class="home-services-block">
                 <div class="flex-wrap">
                     <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/digital-icon.svg'; ?>" alt="Digital">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Digital</h3>
@@ -88,7 +116,7 @@
                     <li class="rev-bullet"><span>Security and maintenance</span></li>
                 </ul>
             </div>
-            <div class="services-block">
+            <div class="home-services-block">
                 <div class="flex-wrap">
                     <img class="rm-logo" src="<?php echo get_template_directory_uri() . '/assets/imgs/print-icon.svg'; ?>" alt="Strategy">
                     <h3 style="margin-right: auto; margin-left: 2rem;">Print</h3>
@@ -105,7 +133,7 @@
 </section>
 <section class="site-width card-text-section">
     <div class="image-text-wrap">
-        <div class="rev-card"></div>
+        <div class="rev-card hp-img-1"></div>
         <div class="card-image-wrap">
             <h3>Bespoke customer journeys with expert <span class="grad-text">Shopify design and development.</span></h3>
             <a class="rev-button">View Projects</a>
@@ -116,10 +144,10 @@
             <h3><span class="grad-text">Wordpress sites</span> to showcase your business and boost online enagement.</h3>
             <a class="rev-button">View Projects</a>
         </div>
-        <div class="rev-card"></div>
+        <div class="rev-card hp-img-2"></div>
     </div>
     <div class="image-text-wrap">
-        <div class="rev-card"></div>
+        <div class="rev-card hp-img-3"></div>
         <div class="card-image-wrap">
             <h3>Straight-talking social media marketing <span class="grad-text">for ambitious brands.</span></h3>
             <a class="rev-button">View Projects</a>
