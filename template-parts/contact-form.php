85844044adb0bbe8b2869d56453fd552d5475ac3
madebyidol <webmaster@revivalmedia.co.uk>
2022-01-31 10:23:49 -0700

    Inserted HTML Forms plugin shortcode.

--- template-parts/contact-form.php
+++ template-parts/contact-form.php
@@ -3,25 +3,7 @@
         <div class="contact-card-text">
             <h3>Like the idea of working with us?</h3>
             <h1>Book an <span class="grad-text">intro call</span> today.</h1>
-            <form class="contact-form contact-form-template" action="" style="margin-bottom: 0 !important;">
-                <div class="field">
-                    <label for="">Name *</label>
-                    <input type="text" placeholder="Test card text input">
-                </div>
-                <div class="field">
-                    <label for="">Email *</label>
-                    <input type="text" placeholder="Test card text input">
-                </div>
-                <div class="field">
-                    <label for="">Job Title</label>
-                    <input type="text" placeholder="Test card text input">
-                </div>
-                <div class="field">
-                    <label for="">Company</label>
-                    <input type="text" placeholder="Test card text input">
-                </div>
-                <a class="rev-button">Send</a>
-            </form>
+            <?php echo do_shortcode('[hf_form slug="book-an-intro-call"]'); ?> <!-- form shortcode, configured with the HTML Forms plugin -->
         </div>
         <img class="contact-template-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/contact-template.jpg'; ?>" alt="An informal zoom meeting.">
     </div>
