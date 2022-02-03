985aa3a47fa4456af93f54e0ca7be1635a75d8bf
madebyidol <webmaster@revivalmedia.co.uk>
2022-02-03 13:57:57 -0700

    Added metafields to single projects template. Styling tweaks.

--- single-projects.php
+++ single-projects.php
@@ -36,13 +36,111 @@
             <div class="yellow screen-ui-btn"></div>
             <div class="green screen-ui-btn"></div>
         </div>
-        <iframe style="width: 100%; height: 600px; overflow: hidden;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Faod1qFZ5p5Ul87qMgd06RB%2FPrecision-Prep-Prototype-Master%3Fpage-id%3D117%253A1063%26node-id%3D963%253A9490%26viewport%3D241%252C48%252C0.09%26scaling%3Dscale-down%26starting-point-node-id%3D963%253A9490" width="100%" height="100%" scrolling="yes" frameBorder="0"></iframe>
+        <iframe style="width: 100%; height: 600px; overflow: hidden;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Faod1qFZ5p5Ul87qMgd06RB%2FPrecision-Prep-Prototype-Master%3Fpage-id%3D1534%253A9717%26node-id%3D1534%253A10214%26viewport%3D241%252C48%252C0.16%26scaling%3Dscale-down-width" width="100%" height="100%" scrolling="yes" frameBorder="0"></iframe>
     </div>
     <div class="screen-wrap">
         <div class="screen-ui">
         </div>
-        <iframe style="width: 100%; height: 600px; overflow: hidden;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Faod1qFZ5p5Ul87qMgd06RB%2FPrecision-Prep-Prototype-Master%3Fpage-id%3D117%253A1063%26node-id%3D963%253A9490%26viewport%3D241%252C48%252C0.09%26scaling%3Dscale-down%26starting-point-node-id%3D963%253A9490" width="100%" height="100%" scrolling="yes" frameBorder="0"></iframe>
+        <iframe style="width: 100%; height: 600px; overflow: hidden;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2Faod1qFZ5p5Ul87qMgd06RB%2FPrecision-Prep-Prototype-Master%3Fpage-id%3D1534%253A9717%26node-id%3D1534%253A9718%26viewport%3D241%252C48%252C0.63%26scaling%3Dscale-down-width" width="100%" height="100%" scrolling="yes" frameBorder="0"></iframe>
     </div>
+</section>
+<section class="feature-section site-width">
+    <div class="image-text-wrap">
+        <div class="rev-card hp-img-1"></div>
+        <div class="card-image-wrap">
+            <h3>Bespoke customer journeys with expert <span class="grad-text">Shopify design and development.</span></h3>
+            <a class="rev-button">View Projects</a>
+        </div>
+    </div>
+    <div class="image-text-wrap card-switch">
+        <div style="text-align: right;" class="card-image-wrap">
+            <h3><span class="grad-text">Wordpress sites</span> to showcase your business and boost online enagement.</h3>
+            <a class="rev-button">View Projects</a>
+        </div>
+        <div class="rev-card hp-img-2"></div>
+    </div>
+    <div class="image-text-wrap">
+        <div class="rev-card hp-img-3"></div>
+        <div class="card-image-wrap">
+            <h3>Straight-talking social media marketing <span class="grad-text">for ambitious brands.</span></h3>
+            <a class="rev-button">View Projects</a>
+        </div>
+    </div>
+</section>
+<section class="site-width" style="padding-bottom: 3rem;">
+    <h2>Some of our <span class="grad-text">other projects.</span></h2>
+</section>
+<section class="related-projects-section site-width">
+    <div class="project-page-glide">
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+                <?php
+                    // Define Query Parameters
+                    $project_posts = new WP_Query( 'post_type=projects' );
+
+                    // Start WP Query
+                    while ($project_posts -> have_posts()) : $project_posts -> the_post(); 
+                ?>
+                <li class="glide__slide">
+                    <div class="hero-card-link-wrap">
+                        <a href="<?php the_permalink() ?>">
+                            <div style="background-color: <?php echo $background; ?>;" class="rev-card hero-card" id="card-<?php echo $post->ID; ?>">
+                                <div>
+                                    <h4 style="color: <?php echo $textcolor; ?>;"><?php echo $clientName; ?></h4>
+                                    <h2 style="color: <?php echo $textcolor; ?>;"><?php echo $projectName; ?></h2>
+                                    <div style="margin-top: 1.5rem;" class="rev-button">Discover More</div>
+                                </div>
+                                <div class="screen-wrap">
+                                    <div class="screen-ui">
+                                        <div class="red screen-ui-btn"></div>
+                                        <div class="yellow screen-ui-btn"></div>
+                                        <div class="green screen-ui-btn"></div>
+                                    </div>
+                                    <div style="background-image: url(<?php echo $desktop; ?>);" class="screenshot"></div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <?php 
+                    // Repeat the process and reset once it hits the limit
+                    endwhile;
+                    wp_reset_postdata();
+                ?>
+            </ul>
+        </div>
+    </div>
+    <!-- GlideJS script -->
+    <script>
+        var glide = new Glide('.project-page-glide', {
+            type: 'carousel',
+            perView: 3,
+            perTouch: 3,
+            focusAt: 0,
+            gap: 20,
+            autoplay: 8000,
+            animationDuration: 150,
+            peek: 150,
+            breakpoints: {
+                1600: { // Breakpoint at <1600px
+                    perView: 2,
+                    peek: 100
+                },
+                1309: {
+                    perView: 2,
+                    peek: 50
+                },
+                767: {
+                    perView: 1,
+                    peek: 100
+                },
+                575: {
+                    perView: 1,
+                    peek: 25
+                }
+            }
+        }).mount()
+    </script>
 </section>
 
 <?php get_template_part('template-parts/contact-form'); ?>
