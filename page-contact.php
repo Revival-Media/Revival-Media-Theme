a2ecadf1ba243670e0aabd098754deeb5579168c
madebyidol <webmaster@revivalmedia.co.uk>
2022-02-03 14:15:59 -0700

    Reordered the contact page's HTML elements.

--- page-contact.php
+++ page-contact.php
@@ -8,6 +8,10 @@
     <p>Pop your details into the form and a member of our team will get back to you as soon as we can.</p>
 </section>
 <section class="site-width contact-wrap">
+    <div class="rev-card form-card">
+        <h2>Book an <span class="grad-text">intro call</span> today.</h2>
+        <?php echo do_shortcode('[hf_form slug="book-an-intro-call"]'); ?> <!-- form shortcode, configured with the HTML Forms plugin -->
+    </div>
     <div class="rev-card map-card">
         <iframe src="https://snazzymaps.com/embed/362847" width="100%" height="300px"></iframe>
         <div class="address-wrap">
@@ -28,10 +32,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="rev-card form-card">
-        <h2>Book an <span class="grad-text">intro call</span> today.</h2>
-        <?php echo do_shortcode('[hf_form slug="book-an-intro-call"]'); ?> <!-- form shortcode, configured with the HTML Forms plugin -->
     </div>
 </section>
 
