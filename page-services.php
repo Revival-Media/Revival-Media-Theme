ef0fd448c73befc63a7c7d8710510cb248a20a95
madebyidol <webmaster@revivalmedia.co.uk>
2022-02-16 11:22:43 -0700

    Almost finished services page copy.

--- page-services.php
+++ page-services.php
@@ -11,17 +11,17 @@
         <div class="service-text">
             <div class="flex-wrap">
                 <img src="<?php echo get_template_directory_uri() . '/assets/imgs/web-dev-icon.svg'; ?>" alt="Development">
-                <h3 style="margin-right: auto; margin-left: 2rem;">Web Development</h3>
+                <h3 style="margin-right: auto; margin-left: 2rem;">Web design and development</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Wordpress development</span></li>
+                    <li class="rev-bullet"><span>Shopify development</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Fully interactive prototypes</span></li>
+                    <li class="rev-bullet"><span>Totally responsive</span></li>
                 </div>
             </ul>
         </div>
@@ -33,15 +33,15 @@
                 <img src="<?php echo get_template_directory_uri() . '/assets/imgs/social-icon.svg'; ?>" alt="Social">
                 <h3 style="margin-right: auto; margin-left: 2rem;">Social media marketing</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>Cut to the chase with our social media services, connecting consumers to the heart of your brand. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Engaging feed content</span></li>
+                    <li class="rev-bullet"><span>Targeted ads and PPC</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Expert social strategy</span></li>
+                    <li class="rev-bullet"><span>Engagement is our bread and butter</span></li>
                 </div>
             </ul>
         </div>
@@ -53,15 +53,15 @@
                 <img style="transform: scale(0.7);" src="<?php echo get_template_directory_uri() . '/assets/imgs/print-icon.svg'; ?>" alt="Print">
                 <h3 style="margin-right: auto; margin-left: 2rem;">Print and digital design</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>Good design is our foundation, be it digital or print. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Brochures, posters and flyers</span></li>
+                    <li class="rev-bullet"><span>Digital graphics and artwork</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Signage and banners</span></li>
+                    <li class="rev-bullet"><span>Digital billboards</span></li>
                 </div>
             </ul>
         </div>
@@ -73,14 +73,14 @@
                 <img src="<?php echo get_template_directory_uri() . '/assets/imgs/brand-build-icon.svg'; ?>" alt="Brand">
                 <h3 style="margin-right: auto; margin-left: 2rem;">Brand building</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>We know a business's identity starts with their brand. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Powerful brand systems</span></li>
+                    <li class="rev-bullet"><span>Written digital and print copy</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
+                    <li class="rev-bullet"><span>Pixel-perfect logos</span></li>
                     <li class="rev-bullet"><span>Commercial goal-setting</span></li>
                 </div>
             </ul>
@@ -91,17 +91,17 @@
         <div class="service-text">
             <div class="flex-wrap">
                 <img src="<?php echo get_template_directory_uri() . '/assets/imgs/copywriting-icon.svg'; ?>" alt="Copywriting">
-                <h3 style="margin-right: auto; margin-left: 2rem;">Voice and copywriting</h3>
+                <h3 style="margin-right: auto; margin-left: 2rem;">Brand voice and copywriting</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>Establishing a unique voice in your industry is challenging, let us help. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Brand voice and copywriting</span></li>
+                    <li class="rev-bullet"><span>Press-releases</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Proof reading and editing</span></li>
+                    <li class="rev-bullet"><span>***We need another one here***</span></li>
                 </div>
             </ul>
         </div>
@@ -113,15 +113,15 @@
                 <img src="<?php echo get_template_directory_uri() . '/assets/imgs/security-icon.svg'; ?>" alt="Security">
                 <h3 style="margin-right: auto; margin-left: 2rem;">Maintenance and security</h3>
             </div>
-            <p>We build first class Wordpress and Shopify sites for a wide range of clients and industries. <a href="#">View projects.</a></p>
+            <p>A website is never finished, only improved. <a href="/work">View projects.</a></p>
             <ul style="margin-left: .7rem; line-height: 2.5rem; display: flex; flex-wrap: wrap;">
                 <div style="margin-right: 2.5rem">
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Complete site and database backups</span></li>
+                    <li class="rev-bullet"><span>Website monitoring</span></li>
                 </div>
                 <div>
-                    <li class="rev-bullet"><span>Innovative creative concepts</span></li>
-                    <li class="rev-bullet"><span>Commercial goal-setting</span></li>
+                    <li class="rev-bullet"><span>Theme, plugin and software updates</span></li>
+                    <li class="rev-bullet"><span>Flexible plans tailored to client requirements</span></li>
                 </div>
             </ul>
         </div>
