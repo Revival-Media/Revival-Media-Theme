5e81ddcf18adb3f71df7273a573591e0e324847a
madebyidol <webmaster@revivalmedia.co.uk>
2022-02-17 17:00:39 -0700

    Copy changed on the contact form.

--- template-parts/contact-form.php
+++ template-parts/contact-form.php
@@ -2,8 +2,9 @@
     <div class="rev-card rev-padding contact-card" style="position: relative;">
         <img class="rev-decore" src="<?php echo get_template_directory_uri() . '/assets/imgs/blob-3.svg'; ?>" style="height: 26rem; left: -5%; top: -22%; transform: rotate(43deg);">
         <div class="contact-card-text">
-            <h3>Like the idea of working with us?</h3>
-            <h1>Book an <span class="grad-text">intro call</span> today.</h1>
+            
+            <h1 style="padding-bottom: 0px;" class="grad-text">Let's talk</h1>
+            <h3 style="padding-bottom: 2.5rem;">Book an <span class="grad-text">intro call</span> with us today.</h3>
             <?php echo do_shortcode('[hf_form slug="book-an-intro-call"]'); ?> <!-- form shortcode, configured with the HTML Forms plugin -->
         </div>
         <img class="contact-template-img" src="<?php echo get_template_directory_uri() . '/assets/imgs/contact-template.jpg'; ?>" alt="An informal zoom meeting.">
