700a0bf143ef7871198be9289a0834c5593fbe92
madebyidol <webmaster@revivalmedia.co.uk>
2022-02-22 08:22:50 -0700

    Added a support page as a test complete with styles.

---
+++ page-support.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="site-width">
+    <div class="support-hero">
+        <h1>Help is here 🔧</h1>
+        <h3>Use the form to report an issue.</h3>
+    </div>
+    <?php echo do_shortcode('[wpsc_create_ticket]'); ?>
+</section>
+
+<?php get_footer(); ?>
