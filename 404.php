34f83e316153bd300a8154be78d326b4245f6342
madebyidol <webmaster@revivalmedia.co.uk>
2022-03-01 04:07:41 -0700

    Created 404 page and added assets.

--- 404.php
+++ 404.php
@@ -10,48 +10,17 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main site-width">
 
 		<section class="error-404 not-found">
-			<header class="page-header">
+
+			<div class="mac-img"></div>		
+			<div class="page-header">
+				<h3 class="grad-text">Error 404</h3>
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'revival-media' ); ?></h1>
-			</header><!-- .page-header -->
+				<a class="rev-button" href="/work">View our work</a>
+			</div><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'revival-media' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'revival-media' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$revival-media_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'revival-media' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$revival-media_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
